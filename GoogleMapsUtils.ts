3a24a089a3b96ffaede5a7b22f8838bc5bfeaa5e
Alex Cordy <acordy@ubriety.com>
2023-03-10 13:44:34 -0500

    Improve map picker and location

--- GoogleMapsUtils.ts
+++ GoogleMapsUtils.ts
@@ -1,36 +1,95 @@
 import { LocationType } from '../../src/common/Info';
 
 export default class GoogleMapsUtils {
-  public static async reverseGeocodeLocation(lat, lng): Promise<LocationType | null> {
-    const geocoder = new google.maps.Geocoder();
-    const response = await geocoder.geocode({ location: new google.maps.LatLng(lat, lng) });
-    const result = response.results?.[0];
-    return result ? GoogleMapsUtils.getLocation(result) : null;
-  }
-
-  public static getLocation(result): LocationType | null {
-    const cities = result.address_components.filter((a) => {
-      return a.types.includes('locality');
+  public static filterGeocoderResponse(
+    results: google.maps.GeocoderResult[],
+    types: string[] = ['neighborhood', 'sublocality', 'locality'],
+  ):
+    {
+      result: google.maps.GeocoderResult,
+      type: string,
+    } | null
+  {
+    let type = null;
+    let result = null;
+    types.every((t) => {
+      result = results.find((a) => {
+        return a.address_components.find((b) => {
+          return b.types.includes(t);
+        });
+      });
+      if (result) {
+        type = t;
+        return false;
+      }
+      return true;
     });
-
-    const regions = result.address_components.filter((a) => {
-      return a.types.includes('administrative_area_level_1');
-    });
-    const countries = result.address_components.filter((a) => {
-      return a.types.includes('country');
-    });
-
-    if (cities.length) {
-      const city = cities[0].long_name;
-      const region = regions[0].short_name;
-      const country = countries[0].long_name;
+    if (result) {
       return {
-        city,
-        region,
-        country,
+        result,
+        type,
       };
     }
-
     return null;
+  }
+
+  public static async reverseGeocodeLocation(lat, lng): Promise<LocationType | null> {
+    const request = {
+      location: new google.maps.LatLng(lat, lng),
+    };
+    const geocoder = new google.maps.Geocoder();
+    const response = await geocoder.geocode(request);
+    const result = GoogleMapsUtils.filterGeocoderResponse(response.results);
+    if (result) {
+      return GoogleMapsUtils.getLocation(result);
+    }
+    return null;
+  }
+
+  public static getLocation(result: { result: google.maps.GeocoderResult, type: string }): LocationType {
+    const lookupOrder = ['sublocality', 'locality', 'administrative_area_level_1', 'country'];
+    const lookupIndexStart = {
+      neighborhood: 0,
+      sublocality: 1,
+      locality: 2,
+    };
+    let lookupIndex = lookupIndexStart[result.type];
+
+    const locationLevel1Component = result.result.address_components.find((a) => {
+      return a.types.includes(result.type);
+    });
+
+    let locationLevel2Component;
+    for (let i = lookupIndex; i < lookupOrder.length; i++) {
+      const found = result.result.address_components.find((a) => {
+        return a.types.includes(lookupOrder[i]);
+      });
+      if (found) {
+        lookupIndex = i + 1;
+        locationLevel2Component = found;
+        break;
+      }
+    }
+
+    let locationLevel3Component;
+    for (let i = lookupIndex; i < lookupOrder.length; i++) {
+      const found = result.result.address_components.find((a) => {
+        return a.types.includes(lookupOrder[i]);
+      });
+      if (found) {
+        locationLevel3Component = found;
+        break;
+      }
+    }
+    const locationLevel2NameType = locationLevel2Component.types.includes('administrative_area_level_1') ? 'short_name' : 'long_name';
+    const locationLevel3NameType = locationLevel3Component.types.includes('administrative_area_level_1') ? 'short_name' : 'long_name';
+    const locationLevel1 = locationLevel1Component.long_name;
+    const locationLevel2 = locationLevel2Component[locationLevel2NameType];
+    const locationLevel3 = locationLevel3Component[locationLevel3NameType];
+    return {
+      locationLevel1,
+      locationLevel2,
+      locationLevel3,
+    };
   }
 }
