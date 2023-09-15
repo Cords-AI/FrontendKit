15961e2142d7d6cdded8f0253305d642e19cab8b
Alex Cordy <acordy@ubriety.com>
2023-09-15 19:19:46 -0400

    Update

---
+++ components/KitButton.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-btn color="primary" no-caps unelevated ref="qRef">
+    <slot></slot>
+    <slot name="loading"></slot>
+  </q-btn>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+const qRef = ref();
+defineExpose({
+  qRef,
+})
+</script>
+
+<style lang="scss" scoped>
+::v-deep(.q-btn) {
+  font-weight: 600;
+  &.bg-primary,
+  &.text-primary {
+    --focus-helper-background: var(--primary-focus-color);
+  }
+  &.bg-secondary,
+  &.text-secondary {
+    --focus-helper-background: var(--secondary-focus-color);
+  }
+  &.bg-positive,
+  &.text-positive {
+    --focus-helper-background: var(--positive-focus-color);
+  }
+  &.bg-warning,
+  &.text-warning {
+    --focus-helper-background: var(--warning-focus-color);
+  }
+  &.bg-negative,
+  &.text-negative {
+    --focus-helper-background: var(--negative-focus-color);
+  }
+  &.q-focusable:focus > .q-focus-helper,
+  &.q-manual-focusable--focused > .q-focus-helper,
+  &.q-hoverable:hover > .q-focus-helper {
+    background: var(--focus-helper-background);
+    opacity: var(--focus-helper-opacity);
+  }
+  .q-focus-helper {
+    &:before,
+    &:after {
+      display: none;
+    }
+  }
+  &.q-btn--rectangle {
+    --focus-helper-opacity: 1;
+    &:not(.q-btn--rounded) {
+      border-radius: 4px;
+    }
+  }
+  &.q-btn--outline,
+  &.q-btn--flat {
+    --focus-helper-opacity: 0.075;
+  }
+}
+</style>
---
+++ components/KitIcon.vue
@@ -0,0 +1,67 @@
+<template>
+  <q-icon ref="qRef" :name="name" :size="sizePx" :style="vfDeclaration">
+    <slot></slot>
+  </q-icon>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, ref } from 'vue';
+
+const qRef = ref();
+defineExpose({
+  qRef,
+})
+
+interface Props {
+  name: string,
+  size?: number|string,
+  outlined?: boolean,
+  rounded?: boolean,
+  sharp?: boolean,
+  weight?: number|string,
+  grade?: number|string,
+  fill?: boolean,
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: 24,
+  outlined: false,
+  rounded: false,
+  sharp: false,
+  weight: 400,
+  grade: 0,
+  fill: false,
+})
+
+let namePrefix;
+if (props.outlined) {
+  namePrefix = 'sym_o_';
+}
+else if (props.rounded) {
+  namePrefix = 'sym_r_';
+}
+else if (props.sharp) {
+  namePrefix = 'sym_s_';
+}
+if (!namePrefix) namePrefix = 'sym_o_';
+const name = `${namePrefix}${props.name}`;
+
+let size = props.size;
+if (typeof size === 'string') {
+  size = size.replace('px', '');
+  size = Number(size);
+}
+const sizePx = `${size}px`;
+
+const opsz = Math.min(Math.max(size, 20), 48);
+const weight = Math.min(Math.max(Number(props.weight), 100), 700);
+const grade = Math.min(Math.max(Number(props.grade), -50), 200);
+const fill = props.fill ? 1 : 0;
+
+const vfValue = `"FILL" ${fill}, "wght" ${weight}, "GRAD" ${grade}, "opsz" ${opsz}`;
+const vfDeclaration = `font-variation-settings: ${vfValue}`;
+
+
+
+
+</script>
